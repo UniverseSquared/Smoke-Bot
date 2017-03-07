dd1ebb5af46ec3ffbc093674bd7f88d3afb77fcf
UniverseSquared <universesquared4@gmail.com>
2017-03-07 16:03:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Discord = require("discord.js"); // Import discord.js
 const bot = new Discord.Client(); // Create the discord client
-const token = "MjUzNTUxMTQzNDc1NjA5NjAw.C5yt7g.fmCFTnh9UQ464Mu7GAbBDKAnrGQ"; // The bot's user token
+const token = "[redacted]"; // The bot's user token
 var prefix = "$"
 
 bot.on("ready", () => {
